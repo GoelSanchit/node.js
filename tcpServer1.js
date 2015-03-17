bfad339df8e135b39d4c2436d47cd423e27729f3
Sanchit Goel <sanchit.mgoel007@gmail.com>
2015-03-17 14:37:19 +0530

    Create tcpServer1.js

---
+++ tcpServer1.js
@@ -0,0 +1,23 @@
+var net = require('net');
+
+var tcpServer = net.createServer(function(socket){
+                console.log('conection established...');
+				
+				socket.on('end', function(){
+				       console.log('server disconnected..');
+					   });
+					   
+				socket.on('close',function(){
+				        console.log('closed event fired');
+						});
+					   
+				socket.on('data', function(data){
+				        console.log('data received from the tcp client');
+					    	socket.write('Server Reply: ' + data);
+						});
+				
+				});
+				
+tcpServer.listen(function(){
+                            var port = tcpServer.address().port;
+							console.log('server started listening on port: ' + port);});
